62821826688703f84d250148ebce295a155730c7
Zubia Tahseen <zubia.tahseen@triosstudent.com>
2026-06-07 19:33:53 -0400

    Initial Commit

---
+++ iOSApp2/iOSApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp2App.swift
+//  iOSApp2
+//
+//  Created by Zubia Tahseen on 2026-06-07.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSApp2Tests/iOSApp2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  iOSApp2Tests.swift
+//  iOSApp2Tests
+//
+//  Created by Zubia Tahseen on 2026-06-07.
+//
+
+import Testing
+@testable import iOSApp2
+
+struct iOSApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
